e05b3f04b46ecfa884ec6859930ab9af1f1df322
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-09-03 15:00:26 +1200

    Remove unused files

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -6,8 +5,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    alias: [
+        {
+            find: /^~(.*)$/,
+            replacement: 'node_modules/$1',
+        },
+    ],
+  },
+  
+
 })
